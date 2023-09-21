0c1bf66c5958fe23565d7dac664dd705df812149
Ana Julia Nunes Melo <100884185+eudirianaju@users.noreply.github.com>
2023-09-21 20:18:45 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $('.carrossel').slick({
+        autoplay: true, // Iniciar a reprodução automática
+        autoplaySpeed: 1000, // Velocidade da reprodução automática (em milissegundos)
+        dots: true, // Mostrar indicadores de navegação (pontos)
+    });
+});
